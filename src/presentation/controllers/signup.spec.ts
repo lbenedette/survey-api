fbbfc4e95513cb2d5672b47bccf0e705ee9ecc9a
Leonardo Lopes <blopesleo@outlook.com>
2021-02-20 14:58:13 -0300

    test: add missing test

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -30,6 +30,20 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 
+  test('Should return 400 with no password is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        name: 'any_name',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
+
   test('Should return 400 with no password confirmation is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
