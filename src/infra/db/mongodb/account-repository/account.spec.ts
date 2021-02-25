bd3c0a01961aea6566f46150a81ce8c1ffc868cf
Leonardo Lopes <blopesleo@outlook.com>
2021-02-25 15:29:16 -0300

    refactor: remove duplicate code

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -68,9 +68,10 @@ describe('Account Mongo Repository', () => {
       email: 'any_email@mail.com',
       password: 'any_password'
     })
-    expect(result.ops[0].accessToken).toBeFalsy()
-    await sut.updateAccessToken(result.ops[0]._id, 'any_token')
-    const account = await accountCollection.findOne({ _id: result.ops[0]._id })
+    const fakeAccount = result.ops[0]
+    expect(fakeAccount.accessToken).toBeFalsy()
+    await sut.updateAccessToken(fakeAccount._id, 'any_token')
+    const account = await accountCollection.findOne({ _id: fakeAccount._id })
     expect(account).toBeTruthy()
     expect(account.accessToken).toBe('any_token')
   })
