6214d5b79a1d84ae44acf173219d8c6015af971a
Leonardo Lopes <blopesleo@outlook.com>
2021-02-26 16:03:16 -0300

    chore: add docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://mongo:27017/clean-node-api',
   port: process.env.PORT ?? 5050,
   jwtSecret: process.env.jwtSecret ?? '$2y$12$fpBjQZDsW4GJoQgbj/2M8eo.4LWXzjU1YX1eqi5yvn0VJO/D2VzUi'
 }
