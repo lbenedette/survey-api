735b2f2e6625e74602906ec86d48dddeced82d2f
Leonardo Lopes <blopesleo@outlook.com>
2021-03-02 17:25:43 -0300

    feat: finish AuthMiddleware

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,5 +1,5 @@
 import { HttpRequest, HttpResponse, Middleware } from '../protocols'
-import { forbidden } from '../helpers/http/http-helper'
+import { forbidden, ok, serverError } from '../helpers/http/http-helper'
 import { AccessDeniedError } from '../errors'
 import { LoadAccountByToken } from '../../domain/use-cases/load-account-by-token'
 
@@ -8,10 +8,17 @@ export class AuthMiddleware implements Middleware {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const accessToken = httpRequest.headers?.['x-access-token']
-    if (accessToken) {
-      await this.loadAccountByAccessToken.load(accessToken)
+    try {
+      const accessToken = httpRequest.headers?.['x-access-token']
+      if (accessToken) {
+        const account = await this.loadAccountByAccessToken.load(accessToken)
+        if (account) {
+          return ok({ accountId: account.id })
+        }
+      }
+      return forbidden(new AccessDeniedError())
+    } catch (error) {
+      return serverError(error)
     }
-    return forbidden(new AccessDeniedError())
   }
 }
